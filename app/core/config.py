959c4d07d43e3a5c66e28c6fd905026bee4503b6
Mario Perez <maperdel@hotmail.com>
2026-06-25 13:57:35 -0600

    Cambio de prueba

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,8 @@ class Settings(BaseSettings):
     JWT_ALG: str = Field(default="HS256", env="JWT_ALG")
     JWT_EXPIRES_MIN: int = Field(default=60 * 24, env="JWT_EXPIRES_MIN")
     PROJECT_NAME: str = "Devinote"
+    ENVIRONMENT: str = Field(..., env="ENVIROMENT")
+    PRUEBA: str = "Esta es una prueba"
 
     class Config:
         env_file = ".env"
